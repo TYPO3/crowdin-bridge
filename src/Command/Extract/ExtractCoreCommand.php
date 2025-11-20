664181fc38bcb85e7a8a9f40ef12d61dd58b4864
Chris Müller <coding@brotkrueml.dev>
2025-11-20 18:11:18 +0100

    Utilize ProjectCollection in extract core command

--- src/Command/Extract/ExtractCoreCommand.php
+++ src/Command/Extract/ExtractCoreCommand.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Command\Extract;
 
-use App\Entity\BridgeConfiguration;
+use App\Configuration\Language;
+use App\Configuration\Project;
+use App\Configuration\ProjectCollection;
 use App\Service\DownloadCrowdinTranslationService;
 use App\Utility\FileHandling;
 use Symfony\Component\Console\Attribute\AsCommand;
@@ -16,34 +18,38 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 #[AsCommand(
     name: 'app:extract:core',
-    description: 'Download translations of TYPO3 core',
-    hidden: false
+    description: 'Download translations of TYPO3 Core',
 )]
-class ExtractCoreCommand extends Command
+final class ExtractCoreCommand extends Command
 {
     public function __construct(
-        protected readonly BridgeConfiguration $bridgeConfiguration,
-        protected DownloadCrowdinTranslationService $downloadCrowdinTranslationService,
-        ?string $name = null
+        private readonly ProjectCollection $projectCollection,
+        private readonly DownloadCrowdinTranslationService $downloadCrowdinTranslationService,
     ) {
-        parent::__construct($name);
+        parent::__construct();
     }
 
     protected function configure()
     {
         $this
-            ->addArgument('language', InputArgument::OPTIONAL, 'List of languages or use "*" for all', '*');
+            ->addArgument('language', InputArgument::OPTIONAL, 'Comma-separated list of languages (for example: de,fr,it) or use "*" for all', '*');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $project = $this->bridgeConfiguration->getProject('typo3-cms');
-
         $io = new SymfonyStyle($input, $output);
         $io->title('TYPO3 Core (typo3-cms)');
 
+        $project = $this->projectCollection->findByIdentifier('typo3-cms');
+        if (!$project instanceof Project) {
+            $io->error('Core project cannot be found!');
+            return Command::FAILURE;
+        }
+
         $languages = $input->getArgument('language') ?? '*';
-        $languageList = $languages === '*' ? $project->getLanguages() : FileHandling::trimExplode(',', $languages, true);
+        $languageList = $languages === '*'
+            ? array_map(static fn(Language $language): string => $language->id, $project->languages)
+            : FileHandling::trimExplode(',', $languages, true);
 
         $this->downloadCrowdinTranslationService->downloadPackageCore($languageList);
 
