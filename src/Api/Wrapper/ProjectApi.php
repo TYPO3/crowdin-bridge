4ab584613ceefb3b98f3c97b288b5a59ed43eafe
Chris Müller <coding@brotkrueml.dev>
2025-11-23 10:23:25 +0100

    Drop unused methods in ProjectApi class
    The skipped projects are already taken care of in the `crowdinSetup` command within the `crowdin-base` library.

--- src/Api/Wrapper/ProjectApi.php
+++ src/Api/Wrapper/ProjectApi.php
@@ -6,42 +6,9 @@ namespace App\Api\Wrapper;
 
 use App\Api\Client;
 use CrowdinApiClient\Model\Progress;
-use CrowdinApiClient\Model\Project;
 
 class ProjectApi extends Client
 {
-    public const array SKIPPED_PROJECTS = ['crowdin-playground-typo3', 'playground-trados'];
-
-    /**
-     * @return Project[]
-     */
-    public function getAll(): array
-    {
-        $projects = [];
-        $items = $this->client->project->list(['limit' => 500]);
-        foreach ($items as $item) {
-            /** @var Project $item */
-            if (in_array($item->getIdentifier(), self::SKIPPED_PROJECTS, true)) {
-                continue;
-            }
-            $projects[$item->getIdentifier()] = $item;
-        }
-        ksort($projects);
-        return $projects;
-    }
-
-    public function getProject(string $extensionKey): ?Project
-    {
-        $projectConfiguration = $this->configuration->getProject($extensionKey);
-
-        return $this->getProjectById($projectConfiguration->getId());
-    }
-
-    public function getProjectById(int $id): ?Project
-    {
-        return $this->client->project->get($id);
-    }
-
     /**
      * @param int $projectId
      * @return Progress[]
